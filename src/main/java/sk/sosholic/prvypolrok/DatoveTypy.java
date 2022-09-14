6fcd22224b1226dd5c4ec5c3d3abe2e503e82132
Stanislav <Palkovic>
2022-09-14 11:12:41 +0200

    .

---
+++ src/main/java/sk/sosholic/prvypolrok/DatoveTypy.java
@@ -0,0 +1,17 @@
+package sk.sosholic.prvypolrok;
+
+public class DatoveTypy {
+
+    public static void main(String[] args) {
+
+        System.out.println("Tu su primitivne datove typy:");
+        System.out.println("int uchova prirodzene cislo velkosti 32b");
+        System.out.println("boolean uchova hodnotu true alebo false");
+        System.out.println("byte uchova prirodzene cislo  velskoti 8b");
+        System.out.println("char uchova charakter");
+        System.out.println("short uchova prirodzene cislo velkosti 16b");
+        System.out.println("long  uchova prirodzene cislo 64b");
+        System.out.println("float uchova realne cislo velkosti 32b");
+        System.out.println("double uchova realne cislo velkosti 64b");
+    }
+}
---
+++ src/main/java/sk/sosholic/prvypolrok/Opakovanie.java
@@ -0,0 +1,9 @@
+package sk.sosholic.prvypolrok;
+
+public class Opakovanie {
+
+    public static void main(String[] args) {
+        System.out.println("Cvičil");
+        System.out.println("Bol s frajerku a este sem byl v grecku");
+    }
+}
